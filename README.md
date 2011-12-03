b387b6f71ea024db14f7d97f9432b89d86d98a1c
Luka Čehovin <luka.cehovin@gmail.com>
2011-12-04 00:38:34 +0100

    Updated admin Bibtex import No url needs to be added for import admin form

--- README.md
+++ README.md
@@ -22,7 +22,6 @@ Installation
 3) Add the following to your project's `urls.py`:
 
 	url(r'^publications/', include('publications.urls')),
-	url(r'^admin/publications/publication/import_bibtex/$', 'publications.admin_views.import_bibtex')
 
 4) Run `python <yourproject>/manage.py syncdb` and `python <yourproject>/manage.py collectstatic`
 
--- publications/admin.py
+++ publications/admin.py
@@ -7,9 +7,14 @@ from django.contrib import admin
 from django import forms
 import publications.models
 from publications.orderedmodel import OrderedModelAdmin
-from publications.models import Publication, Group, Role, Person, PersonNaming, PublicationType, RoleType, Metadata
+from publications.models import Publication, Group, Role, Person, PersonNaming, PublicationType, RoleType, Metadata, generate_publication_objects
 from publications.widgets import PeopleWidget
 from publications.fields import PeopleField
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 def merge_people_by_family_name(modeladmin, request, queryset):
   groups = publications.models.group_people_by_family_name(list(queryset))
@@ -62,22 +67,105 @@ class NamingInline(admin.TabularInline):
 class PublicationAdmin(admin.ModelAdmin):
   radio_fields = {"type": admin.HORIZONTAL}
   raw_id_fields = ["people"]
-	list_display = ('type', 'first_author', 'title', 'year', 'journal_or_book_title')
-	list_display_links = ('title',)
-	change_list_template = 'admin/publications/change_list.html'
-	search_fields = ('title', 'journal', 'authors', 'keywords', 'year')
-	fieldsets = (
-		("Basic information", {'fields': 
-			('type', 'title', 'people_roles', 'abstract', 'note')}),
-		("Publishing information", {'fields': 
-			('year', 'month', 'journal', 'book_title', 'publisher', 'volume', 'number', 'pages')}),
-		("Resources", {'fields': 
-			('url', 'code', 'file', 'doi')}),
-		("Categoritzation", {'fields': 
-			('keywords', 'public', 'groups')}),
-	)
-	inlines = [MetadataInline]
+  list_display = ('type', 'first_author', 'title', 'year', 'journal_or_book_title')
+  list_display_links = ('title',)
+  search_fields = ('title', 'journal', 'authors', 'keywords', 'year')
+  fieldsets = (
+    ("Basic information", {'fields': 
+      ('type', 'title', 'people_roles', 'abstract', 'note')}),
+    ("Publishing information", {'fields': 
+      ('year', 'month', 'journal', 'book_title', 'publisher', 'volume', 'number', 'pages')}),
+    ("Resources", {'fields': 
+      ('url', 'code', 'file', 'doi')}),
+    ("Categoritzation", {'fields': 
+      ('keywords', 'public', 'groups')}),
+  )
+  inlines = [MetadataInline]
   form = PublicationForm
+
+  def import_bibtex(self, request):
+    if request.method == 'POST':
+      # container for error messages
+      errors = list()
+
+      # check for errors
+      if not request.POST['bibliography']:
+        errors.append('This field is required.')
+
+      from publications.bibtex import BibTeXParser, BibTeXProcessor
+
+      bibliography = list()
+      if not errors:
+        parser = BibTeXParser()
+        entries = parser.parse(request.POST['bibliography'])
+        if not entries:
+          for error in parser.getErrors():
+            errors.append("%s (line: %d, column %d)" % (error["message"], error["line"], error["column"]))
+        else:
+          processor = BibTeXProcessor()
+          for entry in entries:
+            processed_entry = processor.process(entry)
+            if not processed_entry:
+              for error in processor.getErrors():
+                errors.append("%s (line: %d, column %d)" % (error["message"], error["line"], error["column"]))
+              continue
+            processed_entry["type"] = entry["type"]
+            bibliography.append(processed_entry)
+
+      if not errors:
+        try:
+          publications = generate_publication_objects(bibliography)
+        except Exception, e:
+          errors.append(str(e))
+
+      if not errors and not publications:
+        errors.append('No valid BibTex entries found.')
+
+      if errors:
+        # some error occurred
+        errors = {"bibliography" : errors}
+        return render_to_response(
+          'admin/publications/publication/import_bibtex.html', {
+            'errors': errors,
+            'title': 'Import BibTex',
+            'request': request},
+          RequestContext(request))
+      else:
+        try:
+          # save publications
+          for publication in publications:
+            publication.save()
+        except:
+          msg = 'Some error occured during saving of publications.'
+        else:
+          if len(publications) > 1:
+            msg = 'Successfully added ' + str(len(publications)) + ' publications.'
+          else:
+            msg = 'Successfully added ' + str(len(publications)) + ' publication.'
+
+        # show message
+        messages.info(request, msg)
+
+        # redirect to publication listing
+        return HttpResponseRedirect(reverse("admin:import_bibtex"))
+    else:
+      return render_to_response(
+        'admin/publications/publication/import_bibtex.html', {
+          'title': 'Import BibTex',
+          'request': request},
+        RequestContext(request))
+
+  def get_urls(self):
+      from django.conf.urls.defaults import patterns, url
+      urls = super(PublicationAdmin, self).get_urls()
+      my_urls = patterns('',
+          url(
+              r'import_bibtex',
+              self.admin_site.admin_view(self.import_bibtex),
+              name='import_bibtex',
+          ),
+      )
+      return my_urls + urls
 
 class GroupAdmin(admin.ModelAdmin):
   list_display = ('identifier', 'title', 'public')
