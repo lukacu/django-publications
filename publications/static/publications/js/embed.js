43aed6b4086550096b96a5674002f0c4421a2628
Luka Čehovin <luka.cehovin@gmail.com>
2012-11-12 14:12:48 +0100

    Fix for IE8 & IE9 embedding

--- publications/static/publications/js/embed.js
+++ publications/static/publications/js/embed.js
@@ -1,5 +1,47 @@
 // -*- Mode: javascript; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
+// Fix for IE8 & IE9 ajax requests (https://raw.github.com/jaubourg/ajaxHooks/master/src/xdr.js) 
+if ( window.XDomainRequest ) {
+	jQuery.ajaxTransport(function( s ) {
+		if ( s.crossDomain && s.async ) {
+			if ( s.timeout ) {
+				s.xdrTimeout = s.timeout;
+				delete s.timeout;
+			}
+			var xdr;
+			return {
+				send: function( _, complete ) {
+					function callback( status, statusText, responses, responseHeaders ) {
+						xdr.onload = xdr.onerror = xdr.ontimeout = jQuery.noop;
+						xdr = undefined;
+						complete( status, statusText, responses, responseHeaders );
+					}
+					xdr = new XDomainRequest();
+					xdr.onload = function() {
+						callback( 200, "OK", { text: xdr.responseText }, "Content-Type: " + xdr.contentType );
+					};
+					xdr.onerror = function() {
+						callback( 404, "Not Found" );
+					};
+					xdr.onprogress = jQuery.noop;
+					xdr.ontimeout = function() {
+						callback( 0, "timeout" );
+					};
+					xdr.timeout = s.xdrTimeout || Number.MAX_VALUE;
+					xdr.open( s.type, s.url );
+					xdr.send( ( s.hasContent && s.data ) || null );
+				},
+				abort: function() {
+					if ( xdr ) {
+						xdr.onerror = jQuery.noop;
+						xdr.abort();
+					}
+				}
+			};
+		}
+	});
+}
+
 // If no jQuery available then no fun
 if (jQuery) {
 
